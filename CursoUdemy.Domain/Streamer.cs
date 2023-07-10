303630a573c98150930d18439e63747fb71909e1
Mario <marioarnedo1@gmail.com>
2023-07-10 22:45:05 +0200

    4. Creacion de modelo

---
+++ CursoUdemy.Domain/Streamer.cs
@@ -0,0 +1,12 @@
+﻿
+namespace CursoUdemy.Domain
+{
+    public class Streamer
+    {
+        public int Id {  get; set; }
+
+        public string? Nombre { get; set; }
+
+        public string? Url { get; set; }
+    }
+}
---
+++ CursoUdemy.Domain/Video.cs
@@ -0,0 +1,13 @@
+﻿namespace CursoUdemy.Domain
+{
+    internal class Video
+    {
+        public int Id { get; set; }
+
+        public string? Nombre { get; set; }
+
+        public int StreamerId { get; set; }
+
+        public virtual Streamer? Streamer { get; set; }
+    }
+}
