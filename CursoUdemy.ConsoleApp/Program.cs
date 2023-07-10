c1a31ee1603b5446e745709d6e8c0ebb2a42b249
Mario <marioarnedo1@gmail.com>
2023-07-10 23:38:01 +0200

    Insercion de contenidos de Entity Framework

--- CursoUdemy.ConsoleApp/Program.cs
+++ CursoUdemy.ConsoleApp/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CursoUdemy.Data;
+using CursoUdemy.Domain;
+
+StreamerDbContext dbContext = new();
+
+Streamer streamer = new(){
+    Nombre = "Amazon Prime",
+    Url = "https://www.primevideo.com"
+};
+
+dbContext!.Streamers!.Add(streamer);
+
+await dbContext.SaveChangesAsync();
+
+var movies = new List<Video>
+{
+    new Video
+    {
+     Nombre = "Mad Max",
+     StreamerId = 1
+    },
+    new Video
+    {
+        Nombre = "El señor de los anillos",
+        StreamerId = streamer.Id
+    },
+    new Video
+    {
+        Nombre = "Crepusculo",
+        StreamerId = streamer.Id
+    },
+    new Video { 
+        Nombre="Manuelita",
+        StreamerId = streamer.Id
+    }
+};
+
+await dbContext.AddRangeAsync(movies);
+
+await dbContext.SaveChangesAsync();
+
--- CursoUdemy.Data/StreamerDbContext.cs
+++ CursoUdemy.Data/StreamerDbContext.cs
@@ -8,7 +8,9 @@ namespace CursoUdemy.Data
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Data Source=localhost\sqlexpress;Initial Catalog=Streamer;Integrated Security=True; TrustServerCertificate=True");
+            optionsBuilder.UseSqlServer(@"Data Source=localhost\sqlexpress;Initial Catalog=Streamer;Integrated Security=True; TrustServerCertificate=True")
+                .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, Microsoft.Extensions.Logging.LogLevel.Information)
+                .EnableSensitiveDataLogging();
         }
         public DbSet <Streamer> Streamers { get; set; }
 
