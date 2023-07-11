5997d1edcfd252d9b333b92a4e28f8a8576d35d0
Mario <marioarnedo1@gmail.com>
2023-07-11 18:31:53 +0200

    Fluet API (usado en caso de que no se sigan los patrones de EF)

--- CursoUdemy.Data/StreamerDbContext.cs
+++ CursoUdemy.Data/StreamerDbContext.cs
@@ -12,6 +12,15 @@ namespace CursoUdemy.Data
                 .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, Microsoft.Extensions.Logging.LogLevel.Information)
                 .EnableSensitiveDataLogging();
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Streamer>().HasMany(m => m.Videos)
+                .WithOne(m => m.Streamer)
+                .HasForeignKey(m => m.StreamerId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+        }
         public DbSet <Streamer> Streamers { get; set; }
 
         public DbSet <Video> Videos { get; set; }
--- CursoUdemy.Domain/Streamer.cs
+++ CursoUdemy.Domain/Streamer.cs
@@ -8,5 +8,7 @@ namespace CursoUdemy.Domain
         public string? Nombre { get; set; }
 
         public string? Url { get; set; }
+
+        public ICollection<Video>? Videos { get; set; }
     }
 }
