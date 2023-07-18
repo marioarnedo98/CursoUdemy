d63bee9bb6365eeb1d123c17f43dccc4a1f6b7e5
Mario <marnedo@emesa.com>
2023-07-18 19:48:34 +0200

    intro a Value Object

--- CursoUdemy.Domain/Common/BaseDomainModel.cs
+++ CursoUdemy.Domain/Common/BaseDomainModel.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
 
-        public DateTime? DateCreated { get; set; }
+        public DateTime? CreatedDate { get; set; }
 
         public string? CreatedBy { get; set; }
 
--- CursoUdemy.Domain/VideoActor.cs
+++ CursoUdemy.Domain/VideoActor.cs
@@ -1,6 +1,8 @@
-﻿namespace CursoUdemy.Domain
+﻿using CursoUdemy.Domain.Common;
+
+namespace CursoUdemy.Domain
 {
-    public class VideoActor
+    public class VideoActor : BaseDomainModel
     {
         public int VideoId { get; set; }
         public int ActorId { get; set; }
