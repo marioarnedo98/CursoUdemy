924ffb0d06518d88555b7ef3e2742d8d034265a8
Mario <marnedo@emesa.com>
2023-07-18 19:32:06 +0200

    añdido auditorias

--- CursoUdemy.Domain/Common/BaseDomainModel.cs
+++ CursoUdemy.Domain/Common/BaseDomainModel.cs
@@ -1,7 +1,14 @@
 ﻿namespace CursoUdemy.Domain.Common
 {
-    public class BaseDomainModel
+    public abstract class BaseDomainModel
     {
         public int Id { get; set; }
+
+        public DateTime? DateCreated { get; set; }
+
+        public string? CreatedBy { get; set; }
+
+        public DateTime LastModifiedDate { get; set; } 
+        public string? LastModifiedBy { get; set;}
     }
 }
